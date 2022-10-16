b2f226c10c2398c4363d961531c35ef6c835d145
Mateus <mateusfilipe2k2@gmail.com>
2022-10-16 11:06:25 -0300

    Update Main.java

--- Coleção de Livros/src/biblioteca/main/Main.java
+++ Coleção de Livros/src/biblioteca/main/Main.java
@@ -25,5 +25,52 @@ public class Main {
 			System.out.println(livro.getTitulo());
 			System.out.println();
 		}
+		
+//		ArrayList<Livro> pesquisa;
+//		pesquisa = Main.pesquisa(livros);
+//
+//		for (Livro livro : pesquisa) {
+//			System.out.println(livro.getClass());
+//			System.out.println(livro.getTitulo());
+//			System.out.println();
+//		}
+	}
+	
+	public static ArrayList<Livro> pesquisa(ArrayList<Livro> livros) {
+
+		ArrayList<Livro> retorno = new ArrayList<Livro>(); 
+		
+		//String tipoMarcado = "Impresso";
+		//String stringPesquisa = "Hobbit";
+		//vetor com os outros itens marcoados ou desmarcados;
+		
+		//Tipo = Tipo da Classe
+//		if(vetorMarcados[0] == "") {
+//			for (Livro livro : livros) {
+//				if(livro.getClass() == tipoMarcado || tipoMarcado == "") {
+//					retorno.add(livro);
+//				}
+//			}
+//		}
+	
+		//Nome = String
+		//Escritor = Array de Strings
+		//Ano de Publicacao = Int
+		//Idioma = String
+		//Palavras Chave = Array de Strings
+		
+		//Livrarias = Array de Strins
+		//Colunas = Int
+		
+		//URL = String
+		//Formato = String
+		
+		//Duracao = Float
+		//FormatoAudio = String
+		
+		//Remove os duplicados do retorno
+		
+		return retorno;
+
 	}
 }
